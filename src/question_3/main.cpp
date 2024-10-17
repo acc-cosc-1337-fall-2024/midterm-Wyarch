5176466c8d1cb8dd51594eb41fb7cf9b90e92dbe
Wyarch <ian.wyatt@g.austincc.edu>
2024-10-17 22:25:23 +0000

    QUESTION 3 COMPLETED

--- src/question_3/main.cpp
+++ src/question_3/main.cpp
@@ -1,6 +1,45 @@
+#include "question3.h"
 
 int main()
 {
+    bool exitFlag = false;
+    bool functionResult;
+    int menuInput;
+    string userStr;
 
+    //Begin Do-While loop
+    do
+    {
+        //Display menu
+        cout<<"Palindrome Test:\n1.Begin Test\n2.Exit Program\n";
+        cin>>menuInput;
+
+        if(menuInput == 1)
+        {
+            cout<<"Testing Palindrome\nEnter your string: \n";
+            cin>>userStr;
+            functionResult = is_palindrome(userStr);
+
+            if (functionResult == 1)
+            {
+                cout<<"Result: True\n\n";
+            }
+            else
+            {
+                cout<<"Result: False\n\n";
+            }
+            
+        }
+        else if(menuInput == 2)
+        {
+            exitFlag = true;
+            cout<<"Exiting Program...\n";
+        }
+        else
+        {
+            cout<<"Invalid input. Please enter an integer from 1 to 2...\n\n";
+        }
+    }while(exitFlag != true);
+    
     return 0;
 }
--- src/question_3/question3.h
+++ src/question_3/question3.h
@@ -3,6 +3,6 @@ bool test_config();
 #include <iostream>
 #include <string>
 
-using std::string;
+using std::string; using std::cout; using std::cin;
 
 bool is_palindrome(string str);
