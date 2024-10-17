806704d578f1e3e75d3a3acc6965a6f72b80e6c8
Wyarch <ian.wyatt@g.austincc.edu>
2024-10-17 22:52:16 +0000

    QUESTION 4 COMPLETE

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,34 @@
+#include "question4.h"
+
 
 int main()
 {
+    bool exitFlag = false;
+    int menuSelection;
+    double celsius;
+    double fahrenheit;
 
+    do
+    {
+        cout<<"Calculate Fahrenheit:\n1.Calculate\n2.Exit\n";
+        cin>>menuSelection;
+
+        if(menuSelection == 1)
+        {
+            cout<<"Calculating celcius...\nEnter celsius: \n";
+            cin>>celsius;
+            fahrenheit = get_fahrenheit(celsius);
+            cout<<"The fahrenheit is: "<<fahrenheit<<"\n\n";
+        }
+        else if(menuSelection == 2)
+        {
+            exitFlag = true;
+            cout<<"Exiting Program...\n";
+        }
+        else
+        {
+            cout<<"Please enter an integeter from 1 or 2...\n\n";
+        }
+    }while(exitFlag != true);
     return 0;
 }
